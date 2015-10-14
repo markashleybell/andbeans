b2a2d0cd37464820c23e64acd40fc0e66a258d3a
Mark Bell <me@markb.co.uk>
2015-10-14 09:02:34 +0100

    Basic image load functionality

--- andbeans/Scripts/main.js
+++ andbeans/Scripts/main.js
@@ -1,24 +1,47 @@
 ﻿var AndBeans = (function ($, window) {
 
-    var _fetch,
-        _init;
+    var _rnd,
+        _fetch,
+        _init,
+        _ui;
+
+    _ui = {
+        search: null,
+        beans: null
+    };
+
+    _rnd = function (min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
 
     _fetch = function (url, data) {
-        return $.ajax({
+        var options = {
             url: url,
-            data: data,
             dataType: 'json',
             type: 'GET'
-        });
+        };
+        if (data) {
+            options.data = data;
+        }
+        return $.ajax(options);
     };
 
     _init = function () {
+        _ui.search = $('div.search');
+        _ui.beans = $('div.beans');
+
         $.when(
-            _fetch('/Main/GetImagesTest', { Query: 'fish' }),
-            _fetch('/Main/GetBeansTest', { Query: 'baked+beans' })
+            _fetch('/Main/GetImages', { Query: 'fish' }),
+            _fetch('/Main/GetBeans')
         ).done(function (data1, data2) {
-            console.log(data1[0]);
-            console.log(data2[0]);
+            var search = data1[0].Results;
+            var beans = data2[0].Results;
+
+            var searchImage = search[_rnd(0, search.length -1)];
+            var beansImage = beans[_rnd(0, beans.length -1)];
+
+            _ui.search.css('background-image', 'url(' + searchImage.MediaUrl + ')');
+            _ui.beans.css('background-image', 'url(' + beansImage.MediaUrl + ')');
         });
     };
 
