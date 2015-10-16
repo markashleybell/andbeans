ca5ebbc23fa56819c5301e10f4c13ced51e72f9c
Mark Bell <me@markb.co.uk>
2015-10-16 08:04:23 +0100

    Rework config code, add protection to cache maintenance methods

---
+++ andbeans/Config.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace andbeans
+{
+    public class Config
+    {
+        public T Get<T>(string key)
+        {
+            // If the key is null (not present in the file)
+            if (ConfigurationManager.AppSettings[key] == null)
+                throw new Exception("Configuration Key Not Found: " + key);
+
+            return (T)Convert.ChangeType(ConfigurationManager.AppSettings[key], typeof(T));
+        }
+    }
+}
--- andbeans/Controllers/MainController.cs
+++ andbeans/Controllers/MainController.cs
@@ -10,18 +10,19 @@ using andbeans.Models.Bing;
 using System.Threading;
 using andbeans.Models;
 using Newtonsoft.Json;
+using System.Net;
 
 namespace andbeans.Controllers
 {
     public class MainController : Controller
     {
-        string _bingApiKey;
+        Config _config;
         DataCache _cache;
         string _cacheKeyPrefix;
 
         public MainController()
         {
-            _bingApiKey = ConfigurationManager.AppSettings["BingApiKey"];
+            _config = new Config();
             _cache = new DataCache();
             _cacheKeyPrefix = "andbeans-";
         }
@@ -31,7 +32,7 @@ namespace andbeans.Controllers
             return View();
         }
 
-        private BingSearchResultContainer FetchData(string query, CacheExpiry expiry)
+        private BingSearchResultContainer FetchData(string query, int expirySeconds)
         {
             var key = _cacheKeyPrefix + "-image-query-" + query;
             var data = _cache.Get<BingSearchResultContainer>(key);
@@ -41,7 +42,7 @@ namespace andbeans.Controllers
             {
                 // Request the data
                 var client = new RestClient("https://api.datamarket.azure.com");
-                client.Authenticator = new HttpBasicAuthenticator("", _bingApiKey);
+                client.Authenticator = new HttpBasicAuthenticator("", _config.Get<string>("BingApiKey"));
 
                 var req = new RestRequest("Bing/Search/v1/Image");
                 req.AddParameter("ImageFilters", "'Size:Large+Aspect:Tall'");
@@ -52,36 +53,42 @@ namespace andbeans.Controllers
                 data = response.Data;
 
                 // And add it to the cache
-                _cache.Add(key, data, (int)expiry);
+                _cache.Add(key, data, expirySeconds);
             }
 
             return data;
         }
 
-        public ActionResult GetImages(string query)
-        {
-            var data = FetchData(query, CacheExpiry.ThirtySeconds);
-            return Json(data.D, JsonRequestBehavior.AllowGet);
-
-            // return Content(System.IO.File.ReadAllText(Server.MapPath("/Tmp/fish.json")), "application/json");
-        }
-
         public ActionResult GetBeans()
         {
-            var data = FetchData("baked beans", CacheExpiry.OneMinute);
+            var data = FetchData("baked beans", _config.Get<int>("BeansCacheTimeSeconds"));
             return Json(data.D, JsonRequestBehavior.AllowGet);
 
             // return Content(System.IO.File.ReadAllText(Server.MapPath("/Tmp/beans.json")), "application/json");
         }
 
-        public ActionResult ClearCacheContents()
+        public ActionResult GetImages(string query)
         {
+            var data = FetchData(query, _config.Get<int>("QueryCacheTimeSeconds"));
+            return Json(data.D, JsonRequestBehavior.AllowGet);
+
+            // return Content(System.IO.File.ReadAllText(Server.MapPath("/Tmp/fish.json")), "application/json");
+        }
+        
+        public ActionResult ClearCacheContents(string key)
+        {
+            if (key != _config.Get<string>("CacheViewKey"))
+                return new HttpNotFoundResult();
+
             _cache.Clear();
-            return Content("CLEARED ALL CACHE");            
+            return Content("CLEARED CACHE");            
         }
 
-        public ActionResult ShowCacheContents()
+        public ActionResult ShowCacheContents(string key)
         {
+            if (key != _config.Get<string>("CacheViewKey"))
+                return new HttpNotFoundResult();
+
             // We filter on Site Key because newer versions of MVC seem to stuff a load of other data into the memory cache 
             // which we're not interested in (break on the line below and inspect _cache.BaseCache to see what I mean...)
             return View(_cache.BaseCache.Where(x => x.Key.StartsWith(_cacheKeyPrefix, StringComparison.Ordinal)).OrderByDescending(x => x.Hits).ToList());
